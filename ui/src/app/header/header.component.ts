d76327512e8a86d3c1b8f8473eaff193cc42e1d0
harveybland <harvey.bland@genius.online>
2021-08-20 14:41:49 +0100

    Background colour

--- ui/src/app/header/header.component.ts
+++ ui/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  // navOpen : boolean = false ; 
+
   constructor() { }
 
   ngOnInit() {
   }
 
 
-  open() {
-    
-  }
+  // open() {
+  //   console.log('hello')
+  //   this.navOpen = !this.navOpen;
+  // }
 }
